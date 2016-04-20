d01f048b6d31f08db5d1a8adde2b9687381af40e
sjbonner <simon.bonner@mail.mcgill.ca>
2016-04-19 22:04:20 -0400

    Changed versioning number to something I can understand.
    Added code to remove records with missing response values. This is the default behaviour.

--- dalmation/R/dalmation.R
+++ dalmation/R/dalmation.R
@@ -18,6 +18,7 @@
 ##' @param residuals If TRUE then compute residuals in output. (logical)
 ##' @param gencode If TRUE then generate code potentially overwriting existing model file. By default generate code if the file does not exist and prompt user if it does. (logical)
 ##' @param drop.levels If TRUE then drop unused levels from all factors in df. (logical)
+##' @param drop.missing If TRUE then remove records with missing response variable. (logical)
 ##'
 ##' @return samples (mcmc.list)
 ##' @author Simon Bonner
@@ -27,196 +28,250 @@ dalmation <- function(df,
                       variance.model,
                       jags.model.args,
                       coda.samples.args,
-                      response=NULL,
-                      rounding=FALSE,
-                      lower=NULL,
-                      upper=NULL,
-                      parameters=NULL,
-                      svd=TRUE,
-                      residuals=FALSE,
-                      gencode=NULL,
-                      drop.levels=TRUE,
-                      debug=FALSE){
+                      response = NULL,
+                      rounding = FALSE,
+                      lower = NULL,
+                      upper = NULL,
+                      parameters = NULL,
+                      svd = TRUE,
+                      residuals = FALSE,
+                      gencode = NULL,
+                      drop.levels = TRUE,
+                      drop.missing = TRUE,
+                      debug = FALSE) {
 
-    if(debug)
-        browser()
+  ## Enter debug state
+  if (debug)
+    browser()
 
-    ## Check that input is sufficient
-    if(rounding && (is.null(lower) || is.null(upper)))
-        stop("If rounding=TRUE then you must specify the names of both the lower and upper bounds of the response.\n\n")
+  ## Check that input is sufficient
+  if (rounding && (is.null(lower) || is.null(upper)))
+    stop(
+      "If rounding=TRUE then you must specify the names of both the lower and upper bounds of the response.\n\n"
+    )
 
-    if(!rounding && is.null(response))
-        stop("Please specify the name of the response variable.\n\n")
+  if (!rounding && is.null(response))
+    stop("Please specify the name of the response variable.\n\n")
 
-    if(is.null(jags.model.args$file))
-        stop("The jags.model.args list must include the name of the model file. Please see help(jags.model) for details.\n\n")
+  if (is.null(jags.model.args$file))
+    stop(
+      "The jags.model.args list must include the name of the model file. Please see help(jags.model) for details.\n\n"
+    )
 
-    if(is.null(coda.samples.args$n.iter))
-        stop("The coda.samples.args list must include a variable n.iter. Please see help(coda.samples) for details.\n\n")
+  if (is.null(coda.samples.args$n.iter))
+    stop(
+      "The coda.samples.args list must include a variable n.iter. Please see help(coda.samples) for details.\n\n"
+    )
 
-    ## Generate JAGS code
-    cat("Step 1: Generating JAGS code...")
+  ## Generate JAGS code
+  cat("Step 1: Generating JAGS code...")
 
-    if(is.null(gencode)){
-      if(!file.exists(jags.model.args$file))
-        gencode <- TRUE
-      else{
+  if (is.null(gencode)) {
+    if (!file.exists(jags.model.args$file))
+      gencode <- TRUE
+    else{
+      tmp <- NULL
 
-        tmp <- NULL
+      while (is.null(tmp)) {
+        tmp <-
+          readline("The model file already exists. Do you want to overwrite it? (y/n)")
 
-        while(is.null(tmp)){
-          tmp <- readline("The model file already exists. Do you want to overwrite it? (y/n)")
-
-          if(tmp == "y" || tmp == "Y")
-            gencode <- TRUE
-          else if(tmp == "n" || tmp == "N")
-            gencode <- FALSE
-          else
-            tmp <- NULL
-        }
+        if (tmp == "y" || tmp == "Y")
+          gencode <- TRUE
+        else if (tmp == "n" || tmp == "N")
+          gencode <- FALSE
+        else
+          tmp <- NULL
       }
     }
+  }
 
-    if(gencode)
-      generateJAGScode(jags.model.args,mean.model,variance.model,rounding=rounding,residuals=residuals)
+  if (gencode)
+    generateJAGScode(
+      jags.model.args,
+      mean.model,
+      variance.model,
+      rounding = rounding,
+      residuals = residuals
+    )
 
-    cat("Done\n")
-
-
-    ## Generate JAGS input data
-    cat("Step 2: Generating JAGS data...")
-
-    jags.model.args$data <- generateJAGSdata(df,mean.model,variance.model,response=response,lower=lower,upper=upper)
-
-    cat("Done\n")
-
-    ## Create useful variable names that will be assigned later
-    mean.names.fixed <- paste0(mean.model$fixed$name,".",
-                               colnames(jags.model.args$data$mean.fixed))
-
-    variance.names.fixed <- paste0(variance.model$fixed$name,".",
-                                   colnames(jags.model.args$data$variance.fixed))
-
-    if(!is.null(mean.model$random))
-        mean.names.random <- paste0(mean.model$random$name,".",
-                                    colnames(jags.model.args$data$mean.random))
-
-    if(!is.null(variance.model$random))
-        variance.names.random <- paste0(variance.model$random$name,".",
-                                        colnames(jags.model.args$data$variance.random))
-
-    ## Perform SVD if requested
-    if(svd){
-        cat("    Computing singular value decompositions to improve mixing...")
-
-        ## Compute SVD
-        mean.fixed.svd <- svd(jags.model.args$data$mean.fixed)
-        variance.fixed.svd <- svd(jags.model.args$data$variance.fixed)
-
-        ## Replace design matrices with orthogal matrices
-        jags.model.args$data$mean.fixed <- mean.fixed.svd$u
-        jags.model.args$data$variance.fixed <- variance.fixed.svd$u
-
-        cat("Done\n")
-    }
-
-    ## Generate JAGS initial values
-    cat("Step 3: Generating initial values...")
-
-    if(is.null(jags.model.args$inits)){
-        if(is.null(jags.model.args$n.chains))
-            cat("\n    Running three parallel chains by default...")
-        jags.model.args$n.chains <- 3
-
-        jags.model.args$inits <- generateJAGSinits(mean.model,variance.model,jags.model.args$data,jags.model.args$n.chains)
-
-        cat("Done\n")
-    }
-    else{
-        if(is.null(jags.model.args$n.chains))
-            jags.model.args$n.chains <- length(jags.model.args$inits)
-
-        cat("Skipped\n")
-    }
-
-    ## Initialize model
-
-    cat("Step 4: Running model in JAGS\n")
-
-    cat("    Initializing model\n")
-    model <- do.call(rjags::jags.model,jags.model.args)
-
-    ## List parameters to monitor
-    if(is.null(parameters))
-        parameters <- c(mean.model$fixed$name,
-                        mean.model$random$name,
-                        variance.model$fixed$name,
-                        variance.model$random$name,
-                        "resid")
-
-    if(!is.null(mean.model$random))
-        parameters <- c(parameters,
-                        paste0("sd.",mean.model$random$name),
-                        "varcomp")
-
-    if(!is.null(variance.model$random))
-        parameters <- c(parameters,
-                        paste0("sd.",variance.model$random$name))
-
-    ## Generate samples
-    cat("   Generating samples\n")
-    coda.samples.args$model <- model
-    coda.samples.args$variable.names <- parameters
-
-    coda <- do.call(rjags::coda.samples,coda.samples.args)
-    cat("Done\n")
-
-    ## Final tidying
-    cat("Step 5: Tidying Output...")
-
-    ## Identify indices of mean and variance parameters in coda output
-    mean.index.fixed <- grep(paste0("^",mean.model$fixed$name),
-                       colnames(coda[[1]]))
-
-    variance.index.fixed <- grep(paste0("^",variance.model$fixed$name),
-                       colnames(coda[[1]]))
-
-    if(!is.null(mean.model$random))
-        mean.index.random <- grep(paste0("^",mean.model$random$name),
-                                  colnames(coda[[1]]))
-
-    if(!is.null(variance.model$random))
-        variance.index.random <- grep(paste0("^",variance.model$random$name),
-                                  colnames(coda[[1]]))
+  cat("Done\n")
 
 
-    ## 1) Transform chains to original scale
-    if(svd){
-        for(i in 1:jags.model.args$n.chains){
-            coda[[i]][,mean.index.fixed] <- t(solve(mean.fixed.svd$d * t(mean.fixed.svd$v),t(coda[[i]][,mean.index.fixed])))
+  ## Generate JAGS input data
+  cat("Step 2: Generating JAGS data...")
 
-            coda[[i]][,variance.index.fixed] <- t(solve(variance.fixed.svd$d * t(variance.fixed.svd$v),t(coda[[i]][,variance.index.fixed])))
-        }
-    }
+  jags.model.args$data <-
+    generateJAGSdata(
+      df,
+      mean.model,
+      variance.model,
+      response = response,
+      lower = lower,
+      upper = upper,
+      drop.levels = drop.levels,
+      drop.missing = drop.missing
+    )
 
-    ## Replace column names in coda with names from formula
-    for(i in 1:length(coda)){
-        colnames(coda[[i]])[mean.index.fixed] <- mean.names.fixed
-        colnames(coda[[i]])[variance.index.fixed] <- variance.names.fixed
+  cat("Done\n")
 
-        if(!is.null(mean.model$random))
-            colnames(coda[[i]])[mean.index.random] <- mean.names.random
+  ## Create useful variable names that will be assigned later
+  mean.names.fixed <- paste0(mean.model$fixed$name,
+                             ".",
+                             colnames(jags.model.args$data$mean.fixed))
 
-        if(!is.null(variance.model$random))
-            colnames(coda[[i]])[variance.index.random] <- variance.names.random
-    }
+  variance.names.fixed <- paste0(variance.model$fixed$name,
+                                 ".",
+                                 colnames(jags.model.args$data$variance.fixed))
+
+  if (!is.null(mean.model$random))
+    mean.names.random <- paste0(mean.model$random$name,
+                                ".",
+                                colnames(jags.model.args$data$mean.random))
+
+  if (!is.null(variance.model$random))
+    variance.names.random <-
+    paste0(
+      variance.model$random$name,
+      ".",
+      colnames(jags.model.args$data$variance.random)
+    )
+
+  ## Perform SVD if requested
+  if (svd) {
+    cat("    Computing singular value decompositions to improve mixing...")
+
+    ## Compute SVD
+    mean.fixed.svd <- svd(jags.model.args$data$mean.fixed)
+    variance.fixed.svd <-
+      svd(jags.model.args$data$variance.fixed)
+
+    ## Replace design matrices with orthogal matrices
+    jags.model.args$data$mean.fixed <- mean.fixed.svd$u
+    jags.model.args$data$variance.fixed <- variance.fixed.svd$u
 
     cat("Done\n")
+  }
 
-    ## Return list of output
-    return(list(mean.model=mean.model,
-                variance.model=variance.model,
-                jags.model.args=jags.model.args,
-                coda.samples.args=coda.samples.args,
-                coda=coda))
+  ## Generate JAGS initial values
+  cat("Step 3: Generating initial values...")
+
+  if (is.null(jags.model.args$inits)) {
+    if (is.null(jags.model.args$n.chains))
+      cat("\n    Running three parallel chains by default...")
+    jags.model.args$n.chains <- 3
+
+    jags.model.args$inits <-
+      generateJAGSinits(mean.model,
+                        variance.model,
+                        jags.model.args$data,
+                        jags.model.args$n.chains)
+
+    cat("Done\n")
+  }
+  else{
+    if (is.null(jags.model.args$n.chains))
+      jags.model.args$n.chains <-
+        length(jags.model.args$inits)
+
+    cat("Skipped\n")
+  }
+
+  ## Initialize model
+
+  cat("Step 4: Running model in JAGS\n")
+
+  cat("    Initializing model\n")
+  model <- do.call(rjags::jags.model, jags.model.args)
+
+  ## List parameters to monitor
+  if (is.null(parameters))
+    parameters <- c(
+      mean.model$fixed$name,
+      mean.model$random$name,
+      variance.model$fixed$name,
+      variance.model$random$name,
+      "resid"
+    )
+
+  if (!is.null(mean.model$random))
+    parameters <- c(parameters,
+                    paste0("sd.", mean.model$random$name),
+                    "varcomp")
+
+  if (!is.null(variance.model$random))
+    parameters <- c(parameters,
+                    paste0("sd.", variance.model$random$name))
+
+  ## Generate samples
+  cat("   Generating samples\n")
+  coda.samples.args$model <- model
+  coda.samples.args$variable.names <- parameters
+
+  coda <- do.call(rjags::coda.samples, coda.samples.args)
+  cat("Done\n")
+
+  ## Final tidying
+  cat("Step 5: Tidying Output...")
+
+  ## Identify indices of mean and variance parameters in coda output
+  mean.index.fixed <- grep(paste0("^", mean.model$fixed$name),
+                           colnames(coda[[1]]))
+
+  variance.index.fixed <-
+    grep(paste0("^", variance.model$fixed$name),
+         colnames(coda[[1]]))
+
+  if (!is.null(mean.model$random))
+    mean.index.random <-
+    grep(paste0("^", mean.model$random$name),
+         colnames(coda[[1]]))
+
+  if (!is.null(variance.model$random))
+    variance.index.random <-
+    grep(paste0("^", variance.model$random$name),
+         colnames(coda[[1]]))
+
+
+  ## 1) Transform chains to original scale
+  if (svd) {
+    for (i in 1:jags.model.args$n.chains) {
+      coda[[i]][, mean.index.fixed] <-
+        t(solve(mean.fixed.svd$d * t(mean.fixed.svd$v), t(coda[[i]][, mean.index.fixed])))
+
+      coda[[i]][, variance.index.fixed] <-
+        t(solve(
+          variance.fixed.svd$d * t(variance.fixed.svd$v),
+          t(coda[[i]][, variance.index.fixed])
+        ))
+    }
+  }
+
+  ## Replace column names in coda with names from formula
+  for (i in 1:length(coda)) {
+    colnames(coda[[i]])[mean.index.fixed] <- mean.names.fixed
+    colnames(coda[[i]])[variance.index.fixed] <-
+      variance.names.fixed
+
+    if (!is.null(mean.model$random))
+      colnames(coda[[i]])[mean.index.random] <-
+      mean.names.random
+
+    if (!is.null(variance.model$random))
+      colnames(coda[[i]])[variance.index.random] <-
+      variance.names.random
+  }
+
+  cat("Done\n")
+
+  ## Return list of output
+  return(
+    list(
+      mean.model = mean.model,
+      variance.model = variance.model,
+      jags.model.args = jags.model.args,
+      coda.samples.args = coda.samples.args,
+      coda = coda
+    )
+  )
 }
--- dalmation/R/generate_jags_data.R
+++ dalmation/R/generate_jags_data.R
@@ -1,58 +1,95 @@
-generateJAGSdata <- function(df,mean.model,variance.model,response=NULL,lower=NULL,upper=NULL,drop.levels=TRUE){
-    ## Generate data list
-    jags.data <- list(n=nrow(df))
-
+generateJAGSdata <-
+  function(df,
+           mean.model,
+           variance.model,
+           response = NULL,
+           lower = NULL,
+           upper = NULL,
+           drop.levels = TRUE,
+           drop.missing = TRUE) {
     ## Remove unused factor level from data frame
-    if(drop.levels)
-       df <- gdata::drop.levels(df,reorder=FALSE)
+    if (drop.levels)
+      df <- gdata::drop.levels(df, reorder = FALSE)
+
+    ## Remove records with missing responses
+    if (is.null(response)) {
+      miss <- intersect(which(!is.finite(df[[upper]])),
+                        which(!is.finite(df[[lower]])))
+    }
+    else if (drop.missing)
+      miss <- which(is.finite(df[[response]]))
+
+    if (length(miss) > 0) {
+      df <- df[-miss,]
+
+      warning("Removed ",
+              length(miss),
+              " observations with missing response values.\n")
+    }
+
+    ## Initialize data list
+    jags.data <- list(n = nrow(df))
 
     ## Construct design matrices for mean model
-    jags.data$mean.fixed <- model.matrix(mean.model$fixed$formula,df)
-    jags.data[[paste0(mean.model$fixed$name,".n")]] <- ncol(jags.data$mean.fixed)
+    jags.data$mean.fixed <-
+      model.matrix(mean.model$fixed$formula, df)
+    jags.data[[paste0(mean.model$fixed$name, ".n")]] <-
+      ncol(jags.data$mean.fixed)
 
-    if(!is.null(mean.model$random)){
-        jags.data$mean.random <- model.matrix(mean.model$random$formula,df)
+    if (!is.null(mean.model$random)) {
+      jags.data$mean.random <- model.matrix(mean.model$random$formula, df)
 
-        tmp <- attr(jags.data$mean.random,"assign")
+      tmp <- attr(jags.data$mean.random, "assign")
 
-        jags.data[[paste0(mean.model$random$name,".ncomponents")]] <- max(tmp)
-        jags.data[[paste0(mean.model$random$name,".neffects")]] <- length(tmp)
-        jags.data[[paste0(mean.model$random$name,".levels")]] <- tmp
+      jags.data[[paste0(mean.model$random$name, ".ncomponents")]] <-
+        max(tmp)
+      jags.data[[paste0(mean.model$random$name, ".neffects")]] <-
+        length(tmp)
+      jags.data[[paste0(mean.model$random$name, ".levels")]] <-
+        tmp
 
-        if(!is.null(mean.model$random$sigma))
-          jags.data[[paste0("sd.",mean.model$random$name)]] <- mean.model$random$sigma^2
+      if (!is.null(mean.model$random$sigma))
+        jags.data[[paste0("sd.", mean.model$random$name)]] <-
+        mean.model$random$sigma ^ 2
     }
 
     ## Construct design matrices for variance model
-    jags.data$variance.fixed <- model.matrix(variance.model$fixed$formula,df)
-    jags.data[[paste0(variance.model$fixed$name,".n")]] <- ncol(jags.data$variance.fixed)
+    jags.data$variance.fixed <-
+      model.matrix(variance.model$fixed$formula, df)
+    jags.data[[paste0(variance.model$fixed$name, ".n")]] <-
+      ncol(jags.data$variance.fixed)
 
-    if(!is.null(variance.model$random)){
-        jags.data$variance.random <- model.matrix(variance.model$random$formula,df)
+    if (!is.null(variance.model$random)) {
+      jags.data$variance.random <-
+        model.matrix(variance.model$random$formula, df)
 
-        tmp <- attr(jags.data$variance.random,"assign")
+      tmp <- attr(jags.data$variance.random, "assign")
 
-        jags.data[[paste0(variance.model$random$name,".ncomponents")]] <- max(tmp)
-        jags.data[[paste0(variance.model$random$name,".neffects")]] <- length(tmp)
-        jags.data[[paste0(variance.model$random$name,".levels")]] <- tmp
+      jags.data[[paste0(variance.model$random$name, ".ncomponents")]] <-
+        max(tmp)
+      jags.data[[paste0(variance.model$random$name, ".neffects")]] <-
+        length(tmp)
+      jags.data[[paste0(variance.model$random$name, ".levels")]] <-
+        tmp
     }
 
     ## Construct weights for variance model
-    if(!is.null(variance.model$weights))
+    if (!is.null(variance.model$weights))
       jags.data$weights <- df[[variance.model$weights]]
 
     ## Add response variable
-    if(!is.null(response))
-        jags.data$y <- df[,response]
-    else if(!is.null(lower) && !is.null(upper)){
-        jags.data$lower <- df[,lower]
-        jags.data$upper <- df[,upper]
-        jags.data$dummy <- rep(1,nrow(df))
+    if (!is.null(response))
+      jags.data$y <- df[, response]
+    else if (!is.null(lower) && !is.null(upper)) {
+      jags.data$lower <- df[, lower]
+      jags.data$upper <- df[, upper]
+      jags.data$dummy <- rep(1, nrow(df))
     }
     else
-        stop("You must either specify the exact response value or both lower and upper bounds for rounding.\n\n")
+      stop(
+        "You must either specify the exact response value or both lower and upper bounds for rounding.\n\n"
+      )
 
     ## Return data list
     jags.data
-}
-
+  }
