5c28914b44fe5d2d91abeee84b9921591055e0fc
Simon Bonner <sbonner6@uwo.ca>
2017-10-13 23:33:27 -0400

    Added missing data_doc.R file.

---
+++ dalmatian/R/data_doc.R
@@ -0,0 +1,20 @@
+#' Pied flycatcher feeding data
+#'
+#' Dataset containing 5795 records of 60 pied flycatchers from 33 nest boxes feeding their nestlings during a brood manipulation experiment.
+#' @format A data frame containing 5795 rows and 17 variables
+#'
+"pfdata"
+
+#' Simulated data for illustrating the use of weights
+#'
+#' Simulated data for illustrating the use of weights in the particular case when the responses are averages of observed with different denominators
+#'
+#'  @format A data frame with 100 rows and 3 columns:
+#'  \describe{
+#'    \item{n}{The number of observations.}
+#'    \item{x}{The common predictor value.}
+#'    \item{y}{The mean response value.}
+#'  }
+#'
+"weights.df"
+
