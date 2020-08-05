b50de17bee55c588b0cb24432602865d5427c255
Simon Bonner <sbonne6@uwo.ca>
2020-08-05 18:00:37 -0400

    Corrected number of iterations.

--- data-raw/negative-binomial-1.R
+++ data-raw/negative-binomial-1.R
@@ -25,10 +25,10 @@ mydisp <- list(fixed = list(name = "psi",
 workingDir <- tempdir()
 
 ## Define list of arguments for jags.model()
-jm.args <- list(file=file.path(workingDir,"nbinom_test_1.R"),n.chains = 3, n.adapt = 100)
+jm.args <- list(file=file.path(workingDir,"nbinom_test_1.R"),n.chains = 3, n.adapt = 1000)
 
 ## Define list of arguments for coda.samples()
-cs.args <- list(n.iter=500,thin=20)
+cs.args <- list(n.iter=5000,thin=20)
 
 ## Run the model using dalmatian
 nbresults <- dalmatian(df=nbinom_data_1,
