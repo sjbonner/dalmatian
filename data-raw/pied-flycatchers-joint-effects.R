01fa5860112632d52506eb91a959c6c41482083f
sjbonner <sbonner6@uwo.ca>
2020-08-19 10:13:00 -0400

    Added parallelization.

--- data-raw/pied-flycatchers-joint-effects.R
+++ data-raw/pied-flycatchers-joint-effects.R
@@ -35,7 +35,7 @@ workingDir <- tempdir()
 jm.args <- list(file=file.path(workingDir,"pied_flycatcher_joint_1_jags.R"),n.adapt=1000)
 
 ## Define list of arguments for coda.samples()
-cs.args <- list(n.iter=5000,thin=20)
+cs.args <- list(n.iter=1000,thin=20)
 
 ## Run the model using dalmatian
 pfresults4 <- dalmatian(df=pfdata,
@@ -49,7 +49,7 @@ pfresults4 <- dalmatian(df=pfdata,
                         upper="upper",
                         residuals = FALSE,
                         run.model = TRUE,
-                        engine = "jags", 
+                        n.cores = 3,
                         overwrite = TRUE,
                         debug=FALSE)
   
