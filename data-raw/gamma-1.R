217b3ad75ffbf134b96da1ea8172e222f7f2e997
Simon Bonner <sbonne6@uwo.ca>
2020-08-20 21:34:12 -0400

    Added file to generate sample output for the test gamma model.

---
+++ data-raw/gamma-1.R
@@ -0,0 +1,53 @@
+## Load packages
+library(devtools)
+
+## Load dalmatian
+devtools::load_all()
+
+## Load negative binomial data
+data(gamma_data_1)
+
+## Define mean and variance objects
+mymean <- list(fixed = list(name = "alpha",
+                            formula = ~x1,
+                            link = "log",
+                            priors = list(c("dnorm",0,.001))),
+               random = list(name = "epsilon",
+                             formula = ~ID - 1))
+
+mydisp <- list(fixed = list(name = "psi",
+                            formula = ~x2,
+                            link = "log",
+                            priors = list(c("dnorm",0,.001))),
+               random = list(name = "xi",
+                             formula = ~ID - 1))
+## Set working directory
+workingDir <- tempdir()
+
+## Define list of arguments for jags.model()
+jm.args <- list(file=file.path(workingDir,"gamma_test_1.R"),n.chains = 3, n.adapt = 1000)
+
+## Define list of arguments for coda.samples()
+cs.args <- list(n.iter=5000,thin=20)
+
+## Run the model using dalmatian
+gresults <- dalmatian(df=gamma_data_1,
+                       family = "gamma",
+                       mean.model=mymean,
+                       dispersion.model=mydisp,
+                       jags.model.args=jm.args,
+                       coda.samples.args=cs.args,
+                       response = "y",
+                       residuals = FALSE,
+                       run.model = TRUE,
+                       engine = "JAGS",
+                       n.cores = 3,
+                       overwrite = TRUE,
+                       saveJAGSinput = workingDir)
+
+## For use on remote server
+## save(gresults,"gresults.RData") 
+
+## For use on local machine within package
+save(gresults,
+     file = file.path(proj_path(),"inst","Gamma_1","gresults.RData"))
