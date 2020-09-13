8f4652abfafb85a7ff58e16c24ded651fa3e6120
Simon Bonner <sbonner6@uwo.ca>
2020-09-12 21:42:21 -0400

    Stored output as separate objects instead of in list.

--- data-raw/pied-flycatchers-2.R
+++ data-raw/pied-flycatchers-2.R
@@ -49,12 +49,15 @@ save(pfmcmc3,
      file = file.path(proj_path(),"data-mcmc","pfmcmc3.RData"))
 
 ## Post-processing
-pfresults3 <- list(
-  convergence = convergence(pfmcmc3),
-  traceplots = traceplots(pfmcmc3, show = FALSE),
-  summary = summary(pfmcmc3),
-  caterpillar = caterpillar(pfmcmc3, show = FALSE),
-  ranef = ranef(pfmcmc3))
+pfconvergence3 <- convergence(pfmcmc3)
+pftraceplots3 <- traceplots(pfmcmc3, show = FALSE)
+pfsummary3 <- summary(pfmcmc3)
+pfcaterpillar3 <- caterpillar(pfmcmc3, show = FALSE)
+pfranef3 <- ranef(pfmcmc3)
 
-save(pfresults3,
+save(pfconvergence3,
+     pftraceplots3,
+     pfsummary3,
+     pfcaterpillar3,
+     pfranef3,
      file = file.path(proj_path(),"inst","pfresults3.RData"))
