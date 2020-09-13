b58409a42e1eb7dacb74df29b3dbf0ab5fcb54db
Simon Bonner <sbonner6@uwo.ca>
2020-09-12 21:23:21 -0400

    Stored results as separate objects instead of in list.

--- data-raw/pied-flycatchers-1.R
+++ data-raw/pied-flycatchers-1.R
@@ -48,20 +48,24 @@ pfmcmc <- dalmatian(df=pfdata,
                        overwrite = TRUE,
                        debug=FALSE)
 
-file <- file.path(proj_path(),"data-mcmc","pfmcmc.RData")
-save(pfmcmc, file = file)
+file <- file.path(proj_path(),"data-mcmc","pfmcmc1.RData")
+save(pfmcmc1, file = file)
 
 ## Post-processing
-pfresults <- list(
-  convergence = convergence(pfmcmc),
-  traceplots = traceplots(pfmcmc, show = FALSE),
-  summary = summary(pfmcmc),
-  caterpillar = caterpillar(pfmcmc, show = FALSE),
-  ranef = ranef(pfmcmc),
-  fitted = predict(pfmcmc,
-                   newdata = pfdata[1:5,]))
+pfconvergence1 <-  convergence(pfmcmc1)
+pftraceplots1 <-  traceplots(pfmcmc1, show = FALSE)
+pfsummary1 <-  summary(pfmcmc1)
+pfcaterpillar1 <-  caterpillar(pfmcmc1, show = FALSE)
+pfranef1 <-  ranef(pfmcmc1)
+pffitted1 <-  predict(pfmcmc1,
+                      newdata = pfdata[1:5,])
 
-save(pfresults,
+save(pfconvergence1,
+     pftraceplots1,
+     pfsummary1,
+     pfcaterpillar1,
+     pfranef1,
+     pffitted1,
      file = file.path(proj_path(),"inst","pfresults.RData"))
 
 ##### Model 2 #####
